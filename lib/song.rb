b23f1e9027505a5a30da8feee7c9e6263ac64085
Annie Truex <artruex@gmail.com>
2019-08-15 21:09:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,8 @@
-class Song 
-  
+class Song
   attr_accessor :name, :artist
 
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
@@ -12,13 +11,8 @@ class Song
   def self.all
     @@all
   end
-  
-    def self.all
-    @@all
-  end
 
   def artist_name
     artist.name if artist
   end
-  
 end
